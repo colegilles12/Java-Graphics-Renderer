6df70adce4e966e6db31426f2983478c923b038f
Cole Gilles <colegilles12>
2023-01-15 20:31:03 -0600

    JavaFX testing

--- src/Renderer/Main.java
+++ src/Renderer/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
 		
-		Window.start();
+		Window window = new Window();
+		
+		window.start();
 		
 	}
 	
--- src/Renderer/Window.java
+++ src/Renderer/Window.java
@@ -1,13 +1,18 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.awt.Graphics2D;
 import java.awt.Graphics;
 
-public class Window {
+public class Window extends JPanel{
 	
-	public static void start() {
-		
+	public void start() {
+
+		this.setSize(Constants.window_height, Constants.window_width);
+
 		JFrame frame = new JFrame();
 		
+		frame.add(this);
+
 		frame.setBounds(100, 100, Constants.window_width, Constants.window_height);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -17,7 +22,7 @@ public class Window {
 	public void paint (Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 			
-		//g2.draw3DRect(0, 0, 100, 100, true);
+		g2.draw3DRect(0, 0, 100, 100, true);
 			
 	}	
 }
