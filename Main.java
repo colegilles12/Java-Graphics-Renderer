5375834374ae72e0ae249aa72d3e3fd186ed8f4a
Cole Gilles <colegilles12>
2023-01-15 19:55:30 -0600

    Opens a blank window

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
+import javax.swing.JFrame;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		
+		JFrame frame = new JFrame();
+		
+		frame.setBounds(100,100,500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
 	}
 	
 }
